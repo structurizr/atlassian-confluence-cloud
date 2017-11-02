0df439f80932044e2a06bc90a81a91c11574643c
Simon Brown <simon.brown@codingthearchitecture.com>
2017-11-02 12:49:48 +0000

    Ditto.

--- docs/structurizr-confluence.js
+++ docs/structurizr-confluence.js
@@ -3,20 +3,6 @@ function getUrlParameter(param) {
     return decodeURIComponent(codedParam);
 }
 
-AP.require("request", function(request) {
-    var pageId = getUrlParameter("pageId");
-    var pageVersion = getUrlParameter("pageVersion");
-    var macroId = getUrlParameter("macroId");
-    request({
-        url: "/rest/api/content/" + pageId +
-        "/history/" + pageVersion +
-        "/macro/id/" + macroId,
-        success: function(response) {
-            embedDiagram(JSON.parse(response));
-        }
-    });
-});
-
 function getMacroParameter(macro, name, defaultValue) {
     if (macro.parameters[name] && macro.parameters[name].value) {
         return macro.parameters[name].value;
@@ -35,3 +21,19 @@ StructurizrEmbed.resizeEmbeddedDiagram = function (elementId) {
     structurizrEmbed.height = height + "px";
     AP.resize("100%", height + "px");
 };
+
+function main() {
+    AP.require("request", function(request) {
+        var pageId = getUrlParameter("pageId");
+        var pageVersion = getUrlParameter("pageVersion");
+        var macroId = getUrlParameter("macroId");
+        request({
+            url: "/rest/api/content/" + pageId +
+            "/history/" + pageVersion +
+            "/macro/id/" + macroId,
+            success: function(response) {
+                embedDiagram(JSON.parse(response));
+            }
+        });
+    });
+}
